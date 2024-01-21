8805421afb681414799cb7d5ec2393c06daa2d6f
warband0 <warband00@gmail.com>
2024-01-21 18:23:09 +0300

    fix: add path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('01-read-file/text.txt');
-readStream.on('data', text => console.log(text.toString()));
+const path = require('path');
+const readStream = fs.createReadStream(path.resolve(__dirname, './text.txt'));
+readStream.on('data', chunk =>{
+    console.log(chunk.toString());
+});
