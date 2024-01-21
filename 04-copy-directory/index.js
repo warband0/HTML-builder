7a7fe2f04e8318441e62b7cfaa75948b6ce57758
warband0 <warband00@gmail.com>
2024-01-21 23:05:41 +0300

    feat: 04-copy-directory  done

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const pathToOrig = path.resolve(__dirname, './files');
+const pathToCopy = path.resolve(__dirname, './files-copy');
+
+function copyDir() {
+  fs.access(pathToCopy, (error) => {
+    if (error) {
+      fsPromises.mkdir(path.resolve(pathToCopy));
+    }
+  });
+  let originalFiles;
+  fs.readdir(pathToOrig, (error, files) => {
+    files.forEach((file) => {
+      fsPromises.copyFile(pathToOrig + `/${file}`, pathToCopy + `/${file}`);
+    });
+    originalFiles = files;
+    fs.readdir(pathToCopy, (error, files) => {
+      files.forEach((file) => {
+        if (!originalFiles.includes(file)) {
+          fs.unlink(pathToCopy + `/${file}`, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    });
+  });
+}
+copyDir();
